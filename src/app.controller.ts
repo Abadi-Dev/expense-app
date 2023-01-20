7fcc920ec0e70bdd76f250b42bb31ce4ba2d534c
Abdulelah Alamri <Abadi_dev@outlook.com>
2023-01-20 19:58:21 +0300

    add other end points

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('/report/:typeOfReport')
 export class AppController {
   @Get()
-  getAllIncomeReports(): any[] {
+  getAllReports(): any[] {
     return [];
   }
+  @Post()
+  createReport(): string {
+    return 'created!';
+  }
   @Get(':id')
-  getReportById(): object {
-    return {};
+  getReportById(): string {
+    return 'here is your report!';
+  }
+  @Put(':id')
+  putReportById(): string {
+    return 'report Updated';
+  }
+  @Delete(':id')
+  deleteReportById(): string {
+    return 'report Deleted';
   }
 }
